225a6cd642c49ab81bc90dd3198a95f6f9fc2df4
TDiaz319 <acupuntista@gmail.com>
2022-09-22 12:42:58 +0200

    comprobar filas

--- candy-react/src/App.js
+++ candy-react/src/App.js
@@ -24,6 +24,21 @@ function App() {
     }
   }
 
+  const checkRowOfFour = () => {
+    for(let i = 0; i<64; i++) {
+      const rowOfFour = [i, i + 1, i + 2]
+      const decidedColor = currentColorArray[i]
+      const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47 ,53, 54, 55, 62, 63, 64]
+
+      if(notValid.includes(i)) continue
+
+      if (rowOfFour.every(square => currentColorArray[square] === decidedColor)) {
+        rowOfFour.forEach(square => currentColorArray[square] ='')
+      }
+    }
+  }
+
+
   const checkColumnOfThree = () => {
     for(let i = 0; i<47; i++) {
       const columnOfThree = [i, i + width, i + width *2]
@@ -35,7 +50,19 @@ function App() {
     }
   }
 
+  const checkRowOfThree = () => {
+    for(let i = 0; i<64; i++) {
+      const rowOfThree = [i, i + 1, i + 2]
+      const decidedColor = currentColorArray[i]
+      const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47 ,54, 55, 63, 64]
 
+      if(notValid.includes(i)) continue
+
+      if (rowOfThree.every(square => currentColorArray[square] === decidedColor)) {
+        rowOfThree.forEach(square => currentColorArray[square] ='')
+      }
+    }
+  }
 
 const createBoard = () => {
   const randomColorArray = []
@@ -54,11 +81,13 @@ useEffect (() => {
   const timer = setInterval (() => {
     checkColumnOfThree()
     checkColumnOfFour()
+    checkRowOfFour ()
+    checkRowOfThree()
     setCurrentColorArray([...currentColorArray])
   },100)
   return () => clearInterval(timer)
 
-}, [checkColumnOfFour, checkColumnOfThree, setCurrentColorArray])
+}, [checkColumnOfFour, checkRowOfFour, checkColumnOfThree, checkRowOfThree, setCurrentColorArray])
 console.log (currentColorArray)
 
   return (
