5ebcd1dd79f554cd43c6791dea0121f0f5b510cd
TDiaz319 <acupuntista@gmail.com>
2022-09-22 12:14:55 +0200

    Comprobar columnas

--- candy-react/src/App.js
+++ candy-react/src/App.js
@@ -13,6 +13,30 @@ const candyColors = [
 function App() {
   const [currentColorArray, setCurrentColorArray] = useState ([])
 
+  const checkColumnOfFour = () => {
+    for(let i = 0; i<39; i++) {
+      const columnOfFour = [i, i + width, i + width *2, + width*3]
+      const decidedColor = currentColorArray[i]
+
+      if (columnOfFour.every(square => currentColorArray[square] === decidedColor)) {
+        columnOfFour.forEach(square => currentColorArray[square] ='')
+      }
+    }
+  }
+
+  const checkColumnOfThree = () => {
+    for(let i = 0; i<47; i++) {
+      const columnOfThree = [i, i + width, i + width *2]
+      const decidedColor = currentColorArray[i]
+
+      if (columnOfThree.every(square => currentColorArray[square] === decidedColor)) {
+        columnOfThree.forEach(square => currentColorArray[square] ='')
+      }
+    }
+  }
+
+
+
 const createBoard = () => {
   const randomColorArray = []
   for (let i =0; i<width * width; i++){
@@ -25,6 +49,16 @@ const createBoard = () => {
 useEffect(() => {
   createBoard()
 }, [])
+
+useEffect (() => {
+  const timer = setInterval (() => {
+    checkColumnOfThree()
+    checkColumnOfFour()
+    setCurrentColorArray([...currentColorArray])
+  },100)
+  return () => clearInterval(timer)
+
+}, [checkColumnOfFour, checkColumnOfThree, setCurrentColorArray])
 console.log (currentColorArray)
 
   return (
